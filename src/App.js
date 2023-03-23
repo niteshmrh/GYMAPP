9458123d02b3f0261c50c03c6b4da20bdf16edc3
niteshmrh <niteshmrh@gmail.com>
2023-03-24 00:47:52 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Hero from './components/Hero';
 import Program from './components/Program';
+import Reasons from './components/Reasons';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <Program />
+      <Reasons/>
     </div>
   );
 }
